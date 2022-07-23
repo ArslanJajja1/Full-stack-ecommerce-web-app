6ef7169bc4b93e77fc95655d579b475c53cadf3a
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-23 15:36:45 +0500

    added axios Category crud requests in frontend

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import {
-  AppstoreOutlined,
-  SettingOutlined,
-  UserOutlined,
-  UserAddOutlined,
+    AppstoreOutlined,
+    SettingOutlined,
+    UserOutlined,
+    UserAddOutlined,
 } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
@@ -13,63 +13,71 @@ import { useDispatch, useSelector } from "react-redux";
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState("home");
-  const { user } = useSelector((state) => ({ ...state }));
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const logout = () => {
-    auth.signOut();
-    dispatch({
-      type: "LOGOUT",
-      payload: null,
-    });
-    navigate("/login");
-  };
-  const handleClick = (e) => {
-    // console.log(e.key);
-    setCurrent(e.key);
-  };
-  let subMenuKey = 0;
-  return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-      <Item key="home" icon={<AppstoreOutlined />}>
-        <Link to="/">Home</Link>
-      </Item>
-
-      {!user && (
-        <>
-          <Item
-            key="register"
-            icon={<UserAddOutlined />}
-            style={{ marginLeft: "auto" }}
-          >
-            <Link to="/register">Register</Link>
-          </Item>
-
-          <Item key="login" icon={<UserOutlined />}>
-            <Link to="/login">Login</Link>
-          </Item>
-        </>
-      )}
-
-      {user && (
-        <>
-          <SubMenu
-            icon={<SettingOutlined />}
-            title={user.email && user.email.split('@')[0]}
-            key={"#" + subMenuKey++}
-            style={{marginLeft:"auto"}}
-          >
-            <Item key="setting:1">Option 1</Item>
-            <Item key="setting:2">Option 2</Item>
-            <Item key="setting:3" onClick={logout}>
-              Logout
+    const [current, setCurrent] = useState("home");
+    const { user } = useSelector((state) => ({ ...state }));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const logout = () => {
+        auth.signOut();
+        dispatch({
+            type: "LOGOUT",
+            payload: null,
+        });
+        navigate("/login");
+    };
+    const handleClick = (e) => {
+        // console.log(e.key);
+        setCurrent(e.key);
+    };
+    let subMenuKey = 0;
+    return (
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+            <Item key="home" icon={<AppstoreOutlined />}>
+                <Link to="/">Home</Link>
             </Item>
-          </SubMenu>
-        </>
-      )}
-    </Menu>
-  );
+
+            {!user && (
+                <>
+                    <Item
+                        key="register"
+                        icon={<UserAddOutlined />}
+                        style={{ marginLeft: "auto" }}
+                    >
+                        <Link to="/register">Register</Link>
+                    </Item>
+
+                    <Item key="login" icon={<UserOutlined />}>
+                        <Link to="/login">Login</Link>
+                    </Item>
+                </>
+            )}
+
+            {user && (
+                <>
+                    <SubMenu
+                        icon={<SettingOutlined />}
+                        title={user.email && user.email.split("@")[0]}
+                        key={"#" + subMenuKey++}
+                        style={{ marginLeft: "auto" }}
+                    >
+                        {user && user.role === "subscriber" && (
+                            <Link to="/user/history">
+                                <Item key="setting:1">Dashboard</Item>
+                            </Link>
+                        )}
+                        {user && user.role === "admin" && (
+                            <Link to="/admin/dashboard">
+                                <Item key="setting:1">Dashboard</Item>
+                            </Link>
+                        )}
+                        <Item key="setting:3" onClick={logout}>
+                            Logout
+                        </Item>
+                    </SubMenu>
+                </>
+            )}
+        </Menu>
+    );
 };
 
 export default Header;
