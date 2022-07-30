e0a56e0699996209a163e265208fa6c52f9cfc79
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-30 12:37:09 +0500

    Product create api request function

---
+++ client/src/functions/product.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const createProduct = async (product, authtoken) => {
+    return await axios.post(`${process.env.REACT_APP_API}/product`, product, {
+        headers: { authtoken },
+    });
+};
