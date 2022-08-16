9ed937684e3d928a7b73f9036c2fca47a6530a0c
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-16 09:17:29 +0500

    product cards design

---
+++ client/src/components/cards/ProductCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Card } from "antd";
+import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import laptop from "../../images/laptop.jpg";
+import { Link } from "react-router-dom";
+const { Meta } = Card;
+
+const ProductCard = ({ product }) => {
+    const { images, title, description, slug } = product;
+    return (
+        <Card
+            cover={
+                <img
+                    src={images && images.length ? images[0].url : laptop}
+                    style={{ height: "200px", objectFit: "cover" }}
+                    className="p-1"
+                />
+            }
+            actions={[
+                <Link to={`/product/${slug}`}>
+                    <EyeOutlined className="text-warning" /> <br />
+                    View Product
+                </Link>,
+                <>
+                    <ShoppingCartOutlined className="text-danger" /> <br />
+                    Add To Cart
+                </>,
+            ]}
+        >
+            <Meta
+                title={title}
+                description={
+                    description.length > 70
+                        ? `${description.substring(0, 70)}...`
+                        : description
+                }
+            />
+        </Card>
+    );
+};
+
+export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ProductCard from "../components/cards/ProductCard";
 import { getProductsByCount } from "../functions/product";
 
 const Home = () => {
@@ -8,11 +9,28 @@ const Home = () => {
         loadAllProducts();
     }, []);
     const loadAllProducts = () => {
-        getProductsByCount(1).then((res) => {
+        setLoading(true);
+        getProductsByCount(3).then((res) => {
             setProducts(res.data);
+            setLoading(false);
         });
     };
-    return <div>{JSON.stringify(products)}</div>;
+    return (
+        <>
+            <div className="jumbotron">
+                {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+            </div>
+            <div className="container">
+                <div className="row">
+                    {products.map((product) => (
+                        <div key={product._id} className="col-md-4">
+                            <ProductCard product={product} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
