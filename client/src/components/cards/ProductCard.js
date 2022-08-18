5d54edb8caa6025ef437f2366bcced6ba1990a87
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-18 15:58:13 +0500

    showing product ratings on home page products and single product page

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -3,40 +3,49 @@ import { Card, Skeleton } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.jpg";
 import { Link } from "react-router-dom";
+import { showAverage } from "../../functions/rating";
+
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
     const { images, title, description, slug } = product;
     return (
-        <Card
-            style={{ minHeight: 300 }}
-            cover={
-                <img
-                    src={images && images.length ? images[0].url : laptop}
-                    style={{ height: "200px", objectFit: "cover" }}
-                    className="p-1"
-                />
-            }
-            actions={[
-                <Link to={`/product/${slug}`}>
-                    <EyeOutlined className="text-warning" /> <br />
-                    View Product
-                </Link>,
-                <>
-                    <ShoppingCartOutlined className="text-danger" /> <br />
-                    Add To Cart
-                </>,
-            ]}
-        >
-            <Meta
-                title={title}
-                description={
-                    description.length > 40
-                        ? `${description.substring(0, 40)}...`
-                        : description
+        <>
+            {product && product.ratings && product.ratings.length > 0 ? (
+                showAverage(product)
+            ) : (
+                <div className="text-center pt-1 pb-3">No rating yet</div>
+            )}
+            <Card
+                style={{ minHeight: 300 }}
+                cover={
+                    <img
+                        src={images && images.length ? images[0].url : laptop}
+                        style={{ height: "200px", objectFit: "cover" }}
+                        className="p-1"
+                    />
                 }
-            />
-        </Card>
+                actions={[
+                    <Link to={`/product/${slug}`}>
+                        <EyeOutlined className="text-warning" /> <br />
+                        View Product
+                    </Link>,
+                    <>
+                        <ShoppingCartOutlined className="text-danger" /> <br />
+                        Add To Cart
+                    </>,
+                ]}
+            >
+                <Meta
+                    title={title}
+                    description={`${
+                        description.length > 40
+                            ? `${description.substring(0, 40)}...`
+                            : description
+                    } `}
+                />
+            </Card>
+        </>
     );
 };
 
--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -8,6 +8,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ProductListItems from "./ProductListItems";
 import StarRatings from "react-star-ratings";
 import RatingModal from "../modal/RatingModal";
+import { showAverage } from "../../functions/rating";
 const { TabPane } = Tabs;
 
 const SingleProduct = ({ product, onStarClick, star }) => {
@@ -50,7 +51,11 @@ const SingleProduct = ({ product, onStarClick, star }) => {
             </div>
             <div className="col-md-5">
                 <h2 className="bg-info p-3 text-white">{title && title}</h2>
-
+                {product && product.ratings && product.ratings.length > 0 ? (
+                    showAverage(product)
+                ) : (
+                    <div className="text-center pt-1 pb-3">No rating yet</div>
+                )}
                 <Card
                     actions={[
                         <>
