c20a478ff2d836457b92b02831fd2665aaabff1a
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-20 12:22:27 +0500

    showing categories on the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import LoadingCard from "../components/cards/LoadingCard";
 import ProductCard from "../components/cards/ProductCard";
+import CategoryList from "../components/category/CategoryList";
 import BestSellers from "../components/home/BestSellers";
 import NewArrivals from "../components/home/NewArrivals";
 import { getProducts } from "../functions/product";
@@ -23,6 +24,10 @@ const Home = () => {
                 Top Sellers
             </h4>
             <BestSellers />
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron ">
+                Categories
+            </h4>
+            <CategoryList />
         </div>
     );
 };
