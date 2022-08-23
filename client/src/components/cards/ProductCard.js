80738ab48907cae4bc7f073217db0441b4248bde
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-23 13:42:37 +0500

    showing product price with product title

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -8,7 +8,7 @@ import { showAverage } from "../../functions/rating";
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
-    const { images, title, description, slug } = product;
+    const { images, title, description, slug, price } = product;
     return (
         <>
             {product && product.ratings && product.ratings.length > 0 ? (
@@ -37,7 +37,7 @@ const ProductCard = ({ product }) => {
                 ]}
             >
                 <Meta
-                    title={title}
+                    title={`${title} - $${price}`}
                     description={`${
                         description.length > 40
                             ? `${description.substring(0, 40)}...`
