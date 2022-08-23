67dac6417f4fe6faa0f6b4fc458630e6d54b1ad5
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-23 13:37:15 +0500

    filter product by price feature

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -5,11 +5,17 @@ import {
 } from "../functions/product";
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/cards/ProductCard";
+import { Menu, Slider } from "antd";
+import { DollarOutlined } from "@ant-design/icons";
+const { SubMenu, ItemGroup } = Menu;
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [price, setPrice] = useState([0, 0]);
+    const [ok, setOk] = useState(false);
     const { search } = useSelector((state) => ({ ...state }));
+    const dispatch = useDispatch();
     const { text } = search;
     //1. Load products by default on page load;
     const loadAllProducts = () => {
@@ -34,12 +40,49 @@ const Shop = () => {
         }, 300);
         return clearTimeout(delayed);
     }, [text]);
-
+    //3. Load products based on price range
+    useEffect(() => {
+        fetchProducts({ price });
+    }, [ok]);
+    const handleSlider = (value) => {
+        dispatch({
+            type: "SEARCH_QUERY",
+            payload: { text: "" },
+        });
+        setPrice(value);
+        setTimeout(() => {
+            setOk(!ok);
+        }, 300);
+    };
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-md-3">sidebar</div>
-                <div className="col-md-9">
+                <div className="col-md-3 pt-2">
+                    <h4>Search/Filter</h4>
+                    <hr />
+                    <Menu mode="inline" defaultOpenKeys={["1", "2"]}>
+                        <SubMenu
+                            key="1"
+                            title={
+                                <span className="h6">
+                                    <DollarOutlined /> Price
+                                </span>
+                            }
+                        >
+                            <div>
+                                <Slider
+                                    className="ml-4 mr-4"
+                                    tipFormatter={(v) => `$${v}`}
+                                    range
+                                    value={price}
+                                    onChange={handleSlider}
+                                    max="3000"
+                                />
+                            </div>
+                        </SubMenu>
+                    </Menu>
+                </div>
+                <div className="col-md-9 pt-2">
                     {loading ? (
                         <h4 className="text-danger">Loading...</h4>
                     ) : (
