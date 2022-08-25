c90941d996153e0f06698e676a358c30cf7cefa1
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-25 21:04:33 +0500

    helper method for filter products by shipping,color,brand

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -213,8 +213,27 @@ const handleSub = async (req, res, sub) => {
         .populate("subs", "_id name");
     res.json(products);
 };
+const handleShipping = async (req, res, shipping) => {
+    const products = await Product.find({ shipping })
+        .populate("category", "_id name")
+        .populate("subs", "_id name");
+    res.json(products);
+};
+const handleColor = async (req, res, color) => {
+    const products = await Product.find({ color })
+        .populate("category", "_id name")
+        .populate("subs", "_id name");
+    res.json(products);
+};
+const handleBrand = async (req, res, brand) => {
+    const products = await Product.find({ brand })
+        .populate("category", "_id name")
+        .populate("subs", "_id name");
+    res.json(products);
+};
 exports.searchFilters = async (req, res) => {
-    const { query, price, category, stars, sub } = req.body;
+    const { query, price, category, stars, sub, shipping, color, brand } =
+        req.body;
     if (query) {
         console.log("query", query);
         await handleQuery(req, res, query);
@@ -233,5 +252,14 @@ exports.searchFilters = async (req, res) => {
     }
     if (sub) {
         await handleSub(req, res, sub);
+    }
+    if (shipping) {
+        await handleShipping(req, res, shipping);
+    }
+    if (color) {
+        await handleColor(req, res, color);
+    }
+    if (brand) {
+        await handleBrand(req, res, brand);
     }
 };
