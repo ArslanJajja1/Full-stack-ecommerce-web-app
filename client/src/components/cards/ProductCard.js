ab0892bc3cd87af6ed2ad700b78c760173931834
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-26 12:06:50 +0500

    add to cart handler function added

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -4,11 +4,25 @@ import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.jpg";
 import { Link } from "react-router-dom";
 import { showAverage } from "../../functions/rating";
-
+import _ from "lodash";
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
     const { images, title, description, slug, price } = product;
+    const handleAddToCart = () => {
+        let cart = [];
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem("cart")) {
+                cart = JSON.parse(localStorage.getItem("cart"));
+            }
+            cart.push({
+                ...product,
+                count: 1,
+            });
+            let unique = _.uniqueWith(cart, _.isEqual);
+            localStorage.setItem("cart", JSON.stringify(unique);
+        }
+    };
     return (
         <>
             {product && product.ratings && product.ratings.length > 0 ? (
@@ -30,10 +44,10 @@ const ProductCard = ({ product }) => {
                         <EyeOutlined className="text-warning" /> <br />
                         View Product
                     </Link>,
-                    <>
+                    <a onClick={handleAddToCart}>
                         <ShoppingCartOutlined className="text-danger" /> <br />
                         Add To Cart
-                    </>,
+                    </a>,
                 ]}
             >
                 <Meta
