b1555372a213850e37c97ed3b5a0eff96014ba63
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-27 10:55:22 +0500

    showing order summary on the cart page

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -5,10 +5,49 @@ import { Link } from "react-router-dom";
 const Cart = () => {
     const { cart, user } = useSelector((state) => ({ ...state }));
     const dispatch = useDispatch();
+    const getTotal = () => {
+        return cart.reduce((currentValue, nextValue) => {
+            return currentValue + nextValue.count * nextValue.price;
+        }, 0);
+    };
     return (
-        <div className="container-fluid">
+        <div className="container-fluid pt-3">
             <div className="row">
-                <h4>Cart</h4>
+                <div className="col-md-8">
+                    <h4>Cart / {cart.length}</h4>
+                    {!cart.length ? (
+                        <p>
+                            No products in cart.{" "}
+                            <Link to="/shop">Continue Shopping</Link>
+                        </p>
+                    ) : (
+                        "Show cart products"
+                    )}
+                </div>
+                <div className="col-md-4">
+                    <h4>Order Summary</h4>
+                    <hr />
+                    <p>Products</p>
+                    {cart.map((c, i) => (
+                        <div key={i}>
+                            <p>
+                                {c.title} x {c.count} = ${c.price * c.count}
+                            </p>
+                        </div>
+                    ))}
+                    <hr />
+                    Total : <b>${getTotal()}</b>
+                    <hr />
+                    {user ? (
+                        <button className="btn btn-sm btn-primary mt-2">
+                            Proceed to checkout
+                        </button>
+                    ) : (
+                        <button className="btn btn-sm btn-primary mt-2">
+                            Login to checkout
+                        </button>
+                    )}
+                </div>
             </div>
         </div>
     );
