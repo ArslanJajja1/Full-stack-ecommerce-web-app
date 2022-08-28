adea94218f05b84e73244f4f9ede6d07089cf147
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-28 12:56:34 +0500

    showing product image on cart pages

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import ModalImage from "react-modal-image";
+import laptop from "../../images/laptop.jpg";
 const ProductCardInCheckout = ({ product }) => {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{ width: "100px", height: "auto" }}>
+                        {product.images.length ? (
+                            <ModalImage
+                                small={product.images[0]}
+                                large={product.images[0]}
+                            />
+                        ) : (
+                            <ModalImage small={laptop} large={laptop} />
+                        )}
+                    </div>
+                </td>
                 <td>{product.title}</td>
                 <td>${product.price}</td>
                 <td>{product.brand}</td>
