b036304e470a3dd67a3a832a920dd240388d831b
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-29 17:08:28 +0500

    showing product image in the cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,32 +1,34 @@
-import React from "react";
-import ModalImage from "react-modal-image";
-import laptop from "../../images/laptop.jpg";
+import React from 'react';
+import ModalImage from 'react-modal-image';
+import laptop from '../../images/laptop.jpg';
 const ProductCardInCheckout = ({ product }) => {
-    return (
-        <tbody>
-            <tr>
-                <td>
-                    <div style={{ width: "100px", height: "auto" }}>
-                        {product.images.length ? (
-                            <ModalImage
-                                small={product.images[0]}
-                                large={product.images[0]}
-                            />
-                        ) : (
-                            <ModalImage small={laptop} large={laptop} />
-                        )}
-                    </div>
-                </td>
-                <td>{product.title}</td>
-                <td>${product.price}</td>
-                <td>{product.brand}</td>
-                <td>{product.color}</td>
-                <td>{product.count}</td>
-                <td>Shipping Icon</td>
-                <td>Delete Icon</td>
-            </tr>
-        </tbody>
-    );
+  console.log('product ', product);
+  const colors = ['Black', 'Brown', 'Silver', 'White', 'Blue'];
+  return (
+    <tbody>
+      <tr>
+        <td>
+          <div style={{ width: '100px', height: 'auto' }}>
+            {product.images.length ? (
+              <ModalImage
+                small={product.images[0].url}
+                large={product.images[0].url}
+              />
+            ) : (
+              <ModalImage small={laptop} large={laptop} />
+            )}
+          </div>
+        </td>
+        <td>{product.title}</td>
+        <td>${product.price}</td>
+        <td>{product.brand}</td>
+        <td>{product.color}</td>
+        <td>{product.count}</td>
+        <td>Shipping Icon</td>
+        <td>Delete Icon</td>
+      </tr>
+    </tbody>
+  );
 };
 
 export default ProductCardInCheckout;
