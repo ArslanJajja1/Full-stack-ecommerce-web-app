9cbe5149e302d6c4589a34e8e0cc4578f4b611eb
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-29 17:30:28 +0500

    update product color in cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import ModalImage from 'react-modal-image';
 import laptop from '../../images/laptop.jpg';
 const ProductCardInCheckout = ({ product }) => {
   console.log('product ', product);
+  const dispatch = useDispatch();
   const colors = ['Black', 'Brown', 'Silver', 'White', 'Blue'];
+  const handleColorChange = (e) => {
+    let cart = [];
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+      }
+      cart.map((p, i) => {
+        if (p._id === product._id) {
+          cart[i].color = e.target.value;
+        }
+      });
+    }
+    console.log('cart updated');
+    localStorage.setItem('cart', JSON.stringify(cart));
+    dispatch({
+      type: 'ADD_TO_CART',
+      payload: cart,
+    });
+  };
   return (
     <tbody>
       <tr>
@@ -22,7 +43,26 @@ const ProductCardInCheckout = ({ product }) => {
         <td>{product.title}</td>
         <td>${product.price}</td>
         <td>{product.brand}</td>
-        <td>{product.color}</td>
+        <td>
+          <select
+            className="form-control"
+            onChange={handleColorChange}
+            name="color"
+          >
+            {product.color ? (
+              <option value={product.color}>{product.color}</option>
+            ) : (
+              'select'
+            )}
+            {colors
+              .filter((c) => c !== product.color)
+              .map((c) => (
+                <option key={`${c}#`} value={c}>
+                  {c}
+                </option>
+              ))}
+          </select>
+        </td>
         <td>{product.count}</td>
         <td>Shipping Icon</td>
         <td>Delete Icon</td>
