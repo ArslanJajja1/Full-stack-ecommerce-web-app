af773e93672d833276766705c04a6977707af991
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-31 18:16:47 +0500

    create,remve,list coupons feature for admin done

--- client/src/pages/admin/coupon/CreateCouponPage.js
+++ client/src/pages/admin/coupon/CreateCouponPage.js
@@ -12,6 +12,7 @@ const CreateCouponPage = () => {
   const [expiry, setExpiry] = useState('');
   const [discount, setDiscount] = useState('');
   const [loading, setLoading] = useState(false);
+  const [coupons, setCoupons] = useState([]);
   const { user } = useSelector((state) => ({ ...state }));
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,13 +23,38 @@ const CreateCouponPage = () => {
         setName('');
         setDiscount('');
         setExpiry('');
+        loadAllCoupons();
         toast.success(`${res.data.name} is created`);
       })
       .catch((err) => {
         console.log(err);
         setLoading(false);
+        toast.error('Error! Try Again.');
       });
   };
+  const handleRemove = (couponId) => {
+    setLoading(true);
+    if (window.confirm('Delete?')) {
+      removeCoupon(couponId, user.token)
+        .then((res) => {
+          loadAllCoupons();
+          setLoading(false);
+          toast.error(`${res.data.name} coupon removed`);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    }
+  };
+  const loadAllCoupons = () => {
+    getCoupons().then((res) => {
+      setCoupons(res.data);
+    });
+  };
+  useEffect(() => {
+    loadAllCoupons();
+  }, []);
   return (
     <div className="container-fluid">
       <div className="row">
@@ -36,7 +62,7 @@ const CreateCouponPage = () => {
           <AdminNav />
         </div>
         <div className="col-md-10">
-          <h4>Coupon</h4>
+          {loading ? <h4 className="text-danger">Loading...</h4> : <h4>Coupon</h4>}
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label className="text-muted">Name</label>
@@ -71,6 +97,30 @@ const CreateCouponPage = () => {
             </div>
             <button className="btn btn-outline-primary">Save</button>
           </form>
+          <br />
+          <h4>{coupons.length} Coupons</h4>
+          <table className="table table-bordered">
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Expiry</th>
+                <th scope="col">Discount</th>
+                <th scope="col">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {coupons.map((c) => (
+                <tr key={c._id}>
+                  <td>{c.name}</td>
+                  <td>{new Date(c.expiry).toLocaleDateString()}</td>
+                  <td>{c.discount}%</td>
+                  <td>
+                    <DeleteOutlined onClick={(e) => handleRemove(c._id)} className="text-danger pointer" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
