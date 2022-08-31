54f3eb76b6cecb033f234cdee1d1fe3b26610271
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-31 15:53:31 +0500

    indentation fix

--- server/server.js
+++ server/server.js
@@ -1,30 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+require('dotenv').config();
 
 const app = express();
 mongoose
-    .connect(process.env.DATABASE_URL)
-    .then(() => {
-        console.log(`Database connected successfully !`);
-    })
-    .catch((error) => {
-        console.log(`Database connection error : ${error}`);
-    });
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log(`Database connected successfully !`);
+  })
+  .catch((error) => {
+    console.log(`Database connection error : ${error}`);
+  });
 // Middlewares
-app.use(morgan("dev"));
-app.use(bodyParser.json({ limit: "2mb" }));
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
 app.use(cors());
 // Route Middlewares
-fs.readdirSync("./routes").map((r) =>
-    app.use("/api", require("./routes/" + r))
-);
+fs.readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
 // Port
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
