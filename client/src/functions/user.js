587be0a7690568251c019215e4c99346595eac49
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-31 13:23:39 +0500

    save user address api request to backend

--- client/src/functions/user.js
+++ client/src/functions/user.js
@@ -10,3 +10,5 @@ export const emptyUserCart = async (authtoken) =>
   await axios.delete(`${process.env.REACT_APP_API}/user/cart`, {
     headers: { authtoken },
   });
+export const saveUserAddress = async (address, authtoken) =>
+  await axios.post(`${process.env.REACT_APP_API}/user/address`, { address }, { headers: { authtoken } });
