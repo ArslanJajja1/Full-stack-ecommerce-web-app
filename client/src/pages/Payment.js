2ef3b3985c0c4e42c5b18fe4a435d04db2238e73
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-01 13:56:22 +0500

    payment page setup with stripe

---
+++ client/src/pages/Payment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+const Payment = () => {
+  return (
+    <div className="container p-5 text-center">
+      <h4>Complete your purchase</h4>
+      <Elements stripe={promise}>
+        <div className="col-md-8 offset-md-2">Stripe checkout component</div>
+      </Elements>
+    </div>
+  );
+};
+
+export default Payment;
