c203f1442c187ea2bc8964f9d20b76737405b975
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-01 14:59:42 +0500

    api request for getting client secret

---
+++ client/src/functions/stripe.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const createPaymentIntent = async (authtoken) => {
+  await axios.post(`${process.env.REACT_APP_API}/create-payment-intent`, {}, { headers: { authtoken } });
+};
