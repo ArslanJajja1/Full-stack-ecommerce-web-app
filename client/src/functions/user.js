551e63a83930d149aed16f307fb9049ea51f170f
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-06 12:43:35 +0500

    showing user orders in user's history

--- client/src/functions/user.js
+++ client/src/functions/user.js
@@ -16,3 +16,7 @@ export const applyCoupon = async (coupon, authtoken) =>
   await axios.post(`${process.env.REACT_APP_API}/user/cart/coupon`, { coupon }, { headers: { authtoken } });
 export const createOrder = async (stripeResponse, authtoken) =>
   await axios.post(`${process.env.REACT_APP_API}/user/order`, { stripeResponse }, { headers: { authtoken } });
+export const getUserOrders = async (authtoken) =>
+  await axios.get(`${process.env.REACT_APP_API}/user/orders`, {
+    headers: { authtoken },
+  });
--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,17 +1,80 @@
-import React from "react";
-import UserNav from "../../components/nav/UserNav";
+import React, { useState, useEffect } from 'react';
+import UserNav from '../../components/nav/UserNav';
+import { getUserOrders } from '../../functions/user';
+import { useSelector, useDispatch } from 'react-redux';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { toast } from 'react-toastify';
 
 const History = () => {
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-2">
-                    <UserNav />
-                </div>
-                <div className="col">user page </div>
-            </div>
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+  useEffect(() => {
+    loadUserOrders();
+  }, []);
+  const loadUserOrders = () =>
+    getUserOrders(user.token).then((res) => {
+      console.log('Show orders', res.data);
+      setOrders(res.data);
+    });
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.product.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === 'Yes' ? (
+                <CheckCircleOutlined style={{ color: 'green' }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: 'red' }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+  const showEachOrders = () =>
+    orders.map((order, i) => (
+      <div key={i} className="m-5 p-3 card">
+        <p>Show payment information</p>
+        {showOrderInTable(order)}
+        <div className="row">
+          <div className="col">
+            <p>PDF Download</p>
+          </div>
         </div>
-    );
+      </div>
+    ));
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <UserNav />
+        </div>
+        <div className="col text-center">
+          <h4>{orders.length > 0 ? 'User orders' : 'No orders'}</h4>
+          {showEachOrders()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default History;
