64f49544465f6ec2b9c817df53a0e31b7cc1e052
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-12 11:30:11 +0500

    showing payment info on user dashboard

--- client/src/components/cards/ShowPaymentInfo.js
+++ client/src/components/cards/ShowPaymentInfo.js
@@ -4,19 +4,37 @@ const ShowPaymentInfo = ({ order }) => {
   return (
     <div>
       <p>
-        <span>Order Id :{order.paymentIntent.id}</span>{' '}
         <span>
-          Amount :
+          <p className="badge bg-info text-white"> Order Id </p>:{order.paymentIntent.id}
+        </span>
+        {' | '}
+        <span>
+          <p className="badge bg-info text-white"> Amount </p>:
           {(order.paymentIntent.amount /= 100).toLocaleString('en-US', {
             style: 'currency',
             currency: 'USD',
           })}
-        </span>{' '}
-        <span>Currency :{order.paymentIntent.currency.toUpperCase()}</span>{' '}
-        <span>Method :{order.paymentIntent.payment_method_types[0]}</span>{' '}
-        <span>Payment :{order.paymentIntent.status.toUpperCase()}</span>{' '}
-        <span>Ordered on :{new Date(order.paymentIntent.created * 1000).toLocaleString()}</span>{' '}
-        <span className="badge bg-primary text-white">Status :{order.orderStatus}</span>{' '}
+        </span>
+        {' | '}
+        <span>
+          <p className="badge bg-info text-white"> Currency </p> :{order.paymentIntent.currency.toUpperCase()}
+        </span>
+        {' | '}
+        <span>
+          <p className="badge bg-info text-white"> Method </p> :{order.paymentIntent.payment_method_types[0]}
+        </span>
+        {' | '}
+        <span>
+          <p className="badge bg-info text-white"> Payment </p> :{order.paymentIntent.status.toUpperCase()}
+        </span>
+        {' | '}
+        <span>
+          <p className="badge bg-info text-white"> Ordered on </p> :
+          {new Date(order.paymentIntent.created * 1000).toLocaleString()}
+        </span>
+        {' | '}
+        <span className="badge bg-primary text-white">Status :{order.orderStatus}</span>
+        {' | '}
       </p>
     </div>
   );
