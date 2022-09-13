7da4f36905c8c18827ee98dd4eb06763cc8602fd
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-13 16:31:36 +0500

    showing orders data in a table

--- client/src/components/order/Orders.js
+++ client/src/components/order/Orders.js
@@ -2,6 +2,40 @@ import React from 'react';
 import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import ShowPaymentInfo from '../cards/ShowPaymentInfo';
 const Orders = ({ orders, handleStatusChange }) => {
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.product.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === 'Yes' ? (
+                <CheckCircleOutlined style={{ color: 'green' }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: 'red' }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
   return (
     <>
       {orders.map((order, i) => (
@@ -26,6 +60,7 @@ const Orders = ({ orders, handleStatusChange }) => {
               </div>
             </div>
           </div>
+          {showOrderInTable(order)}
         </div>
       ))}
     </>
