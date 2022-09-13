5fc1c86fcbe24514e561bf97625251d5848e82ea
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-13 15:15:15 +0500

    changed function name to changeStatus

--- client/src/functions/admin.js
+++ client/src/functions/admin.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const getOrders = async (authtoken) =>
   await axios.get(`${process.env.REACT_APP_API}/admin/orders`, { headers: { authtoken } });
 
-export const orderStatus = async (orderId, orderStatus, authtoken) =>
+export const changeStatus = async (orderId, orderStatus, authtoken) =>
   await axios.put(`${process.env.REACT_APP_API}/admin/order-status`, { orderId, orderStatus }, { headers: { authtoken } });
