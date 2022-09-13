0ca5520498b96ff72fb553ab2ca90be758a02bb2
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-13 15:15:56 +0500

    getting orders,updating status on admin dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,17 +1,35 @@
-import React from "react";
-import AdminNav from "../../components/nav/AdminNav";
+import React, { useState, useEffect } from 'react';
+import AdminNav from '../../components/nav/AdminNav';
+import { changeStatus, getOrders } from '../../functions/admin';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const AdminDashboard = () => {
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-2 ">
-                    <AdminNav />
-                </div>
-                <div className="col">Admin dashboard </div>
-            </div>
+  const [orders, setOrders] = useState([]);
+  const { user } = useSelector((state) => ({ ...state }));
+  useEffect(() => {
+    loadOrders();
+  }, []);
+  const loadOrders = () =>
+    getOrders(user.token).then((res) => {
+      setOrders(res.data);
+    });
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then((res) => {
+      toast.success('Order status updated');
+      loadOrders();
+    });
+  };
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2 ">
+          <AdminNav />
         </div>
-    );
+        <div className="col">Admin dashboard </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
