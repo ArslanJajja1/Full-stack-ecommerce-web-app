b36e53bc183d222572adc7307ada18b60ad96365
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-14 19:17:08 +0500

    added wishlist in user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,31 +1,31 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema;
 
 const userSchema = new mongoose.Schema(
-    {
-        name: String,
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-            index: true,
-        },
-        role: {
-            type: String,
-            default: "subscriber",
-        },
-        cart: {
-            type: Array,
-            default: [],
-        },
-        address: String,
-        // wishlist:[
-        //     {
-        //         type:ObjectId,
-        //         ref:"Product"
-        //     }
-        // ]
+  {
+    name: String,
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      index: true,
     },
-    { timestamps: true }
+    role: {
+      type: String,
+      default: 'subscriber',
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: String,
+    wishlist: [
+      {
+        type: ObjectId,
+        ref: 'Product',
+      },
+    ],
+  },
+  { timestamps: true },
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
