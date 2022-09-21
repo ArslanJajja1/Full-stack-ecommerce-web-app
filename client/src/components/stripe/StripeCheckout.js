6a9105be898074685b624e5f81f4722f115829fe
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-21 15:36:08 +0500

    payment page design and responsiveness update

--- client/src/components/stripe/StripeCheckout.js
+++ client/src/components/stripe/StripeCheckout.js
@@ -36,7 +36,6 @@ const StripeCheckout = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('---------------->', clientSecret);
     setProcessing(true);
     const payload = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
@@ -78,6 +77,7 @@ const StripeCheckout = () => {
 
   const cardStyle = {
     style: {
+      width: '100%',
       base: {
         color: '#32325d',
         fontFamily: 'Arial, sans-serif',
@@ -94,32 +94,30 @@ const StripeCheckout = () => {
     },
   };
   return (
-    <>
+    <div className="p-4 shadow-lg bg-body productCard-container" style={{ backgroundColor: '#2c2c6c' }}>
       {!succeeded && (
         <div>
           {coupon && totalAfterDiscount !== undefined ? (
-            <p className="alert alert-success">{`Coupon applied. Total after discount: $${totalAfterDiscount}`}</p>
+            <p className="alert bg-white text-success font-weight-bold">{`Coupon applied. Total after discount: $${totalAfterDiscount}`}</p>
           ) : (
-            <p className="alert alert-danger">No coupon applied</p>
+            <p className="alert bg-white text-danger font-weight-bold">No coupon applied</p>
           )}
         </div>
       )}
-      <div className="text-center pb-5">
-        <Card
-          cover={<img src={laptop} style={{ height: '200px', objectFit: 'cover', marginBottom: '-50px' }} />}
-          actions={[
-            <>
-              <DollarOutlined className="text-info" /> <br />
-              Total : ${cartTotal}
-            </>,
-            <>
-              <CheckOutlined className="text-info" /> <br />
-              Payable : ${(payable / 100).toFixed(2)}
-            </>,
-          ]}
-        />
+      <div className="text-center">
+        <p className="alert bg-white text-info font-weight-bold d-flex justify-content-between align-items-center">
+          <span>
+            <DollarOutlined className=" pr-2" />
+            Total : ${cartTotal}
+          </span>
+          <span>|</span>
+          <span>
+            <CheckOutlined className=" pr-2" />
+            Payable : ${(payable / 100).toFixed(2)}
+          </span>
+        </p>
       </div>
-      <form id="payment-form " className="stripe-form" onSubmit={handleSubmit}>
+      <form id="payment-form " style={{ minWidth: 'max-content', maxWidth: '100%' }} className="" onSubmit={handleSubmit}>
         <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
         <button className="stripe-button" disabled={processing || disabled || succeeded}>
           <span id="button-text">{processing ? <div className="spinner" id="spinner"></div> : 'Pay'}</span>
@@ -127,14 +125,17 @@ const StripeCheckout = () => {
         <br />
         {error && (
           <div className="card-error" role="alert">
-            {error}
+            <p className="text-danger bg-white">{error}</p>
           </div>
         )}
-        <p className={succeeded ? 'result-message' : 'result-message hidden'}>
-          Payment Successfull. <Link to="/user/history">View order</Link>
+        <p className={succeeded ? 'result-message text-white' : 'result-message hidden'}>
+          Payment Successfull.{' '}
+          <Link to="/user/history" className="font-weight-bold text-white text-decoration-underline">
+            View order
+          </Link>
         </p>
       </form>
-    </>
+    </div>
   );
 };
 
--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -8,12 +8,19 @@ const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 const Payment = () => {
   return (
     <div className="container p-5 text-center">
-      <h4>Complete your purchase</h4>
-      <Elements stripe={promise}>
-        <div className="col-md-8 offset-md-2">
-          <StripeCheckout />
-        </div>
-      </Elements>
+      <h4
+        className="text-white font-weight-bold pb-1 mx-auto mb-3"
+        style={{ letterSpacing: '3px', borderBottom: '5px solid #4db5ff', width: 'fit-content' }}
+      >
+        Complete your purchase
+      </h4>
+      <div className=" shadow-lg bg-body productCard-container mx-auto" style={{ backgroundColor: '#2c2c6c', maxWidth: '25rem' }}>
+        <Elements stripe={promise}>
+          <div className=" mx-auto">
+            <StripeCheckout />
+          </div>
+        </Elements>
+      </div>
     </div>
   );
 };
