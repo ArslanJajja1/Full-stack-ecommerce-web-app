bf493e7777af24bfd84dc0e708c90c26e5b92492
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-24 12:47:13 +0500

    responsive admin sidebar

--- client/src/components/nav/AdminNav.js
+++ client/src/components/nav/AdminNav.js
@@ -1,48 +1,109 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { MenuOutlined } from '@ant-design/icons';
+import { Drawer } from 'antd';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const AdminNav = () => {
-    return (
-        <nav>
-            <ul className="nav flex-column">
-                <li className="nav-item">
-                    <Link to="/admin/dashboard" className="nav-link">
-                        Dashboard
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/admin/product" className="nav-link">
-                        Product
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/admin/products" className="nav-link">
-                        Products
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/admin/category" className="nav-link">
-                        Category
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/admin/subcategory" className="nav-link">
-                        Sub Category
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/admin/coupon" className="nav-link">
-                        Coupons
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/user/password" className="nav-link">
-                        Password
-                    </Link>
-                </li>
-            </ul>
+  const [open, setOpen] = useState(false);
+  const onClose = () => {
+    setOpen(false);
+  };
+  const activeNavClass = (e) => {
+    console.log('Eee..', e);
+  };
+  return (
+    <>
+      <div onClick={() => setOpen(true)} className="d-flex mt-4">
+        <MenuOutlined style={{ color: 'white' }} className="hamburger pr-2" />
+        <p className="pointer text-white font-weight-bold">Menu</p>
+      </div>
+      <Drawer
+        width={200}
+        className="text-white"
+        placement="left"
+        size="default"
+        onClose={onClose}
+        open={open}
+        closable={false}
+        drawerStyle={{ backgroundColor: '#2c2c6c', color: 'white' }}
+      >
+        <nav className="">
+          <ul className="nav flex-column">
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/admin/dashboard"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Dashboard
+              </NavLink>
+            </li>
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/admin/product"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Product
+              </NavLink>
+            </li>
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/admin/products"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Products
+              </NavLink>
+            </li>
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/admin/category"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Category
+              </NavLink>
+            </li>
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/admin/subcategory"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Sub Category
+              </NavLink>
+            </li>
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/admin/coupon"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Coupons
+              </NavLink>
+            </li>
+            <li onClick={activeNavClass} className="nav-item">
+              <NavLink
+                to="/user/password"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link text-white font-weight-bold border-bottom' : 'nav-link text-white font-weight-bold '
+                }
+              >
+                Password
+              </NavLink>
+            </li>
+          </ul>
         </nav>
-    );
+      </Drawer>
+    </>
+  );
 };
 
 export default AdminNav;
