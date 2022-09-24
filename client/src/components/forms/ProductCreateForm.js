50f408052203538828196eb4546d830bb9a3936f
ArslanJajja1 <arslanjajja13@gmail.com>
2022-09-24 10:37:58 +0500

    product create form design improvements

--- client/src/components/forms/ProductCreateForm.js
+++ client/src/components/forms/ProductCreateForm.js
@@ -1,165 +1,152 @@
-import React from "react";
-import { Select } from "antd";
+import React from 'react';
+import { Select } from 'antd';
 const { Option } = Select;
 const ProductCreateForm = ({
-    handleChange,
-    handleSubmit,
-    values,
-    loading,
-    handleCategoryChange,
-    subOptions,
-    showSub,
-    setValues,
+  handleChange,
+  handleSubmit,
+  values,
+  loading,
+  handleCategoryChange,
+  subOptions,
+  showSub,
+  setValues,
 }) => {
-    const {
-        title,
-        description,
-        price,
-        categories,
-        category,
-        subs,
-        shipping,
-        quantity,
-        images,
-        colors,
-        brands,
-        color,
-        brand,
-    } = values;
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    className="form-control"
-                    value={title}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <input
-                    type="text"
-                    name="description"
-                    id="description"
-                    className="form-control"
-                    value={description}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="price">Price</label>
-                <input
-                    type="number"
-                    name="price"
-                    id="price"
-                    className="form-control"
-                    value={price}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="shipping">Shipping</label>
-                <select
-                    name="shipping"
-                    id="shipping"
-                    className="form-control"
-                    onChange={handleChange}
-                >
-                    <option value="No">No</option>
-                    <option value="Yes">Yes</option>
-                </select>
-            </div>
-            <div className="form-group">
-                <label htmlFor="quantity">Quantity</label>
-                <input
-                    type="number"
-                    name="quantity"
-                    id="quantity"
-                    className="form-control"
-                    value={quantity}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label htmlFor="color">Color</label>
-                <select
-                    name="color"
-                    id="color"
-                    className="form-control"
-                    onChange={handleChange}
-                >
-                    <option>Please Select</option>
-                    {colors.map((c) => (
-                        <option key={c} value={c}>
-                            {c}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="form-group">
-                <label htmlFor="brand">Brand</label>
-                <select
-                    name="brand"
-                    id="brand"
-                    className="form-control"
-                    onChange={handleChange}
-                >
-                    <option>Please Select</option>
-                    {brands.map((b) => (
-                        <option key={b} value={b}>
-                            {b}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="form-group">
-                <label htmlFor="category">Category</label>
-                <select
-                    name="category"
-                    id="category"
-                    className="form-control"
-                    onChange={handleCategoryChange}
-                >
-                    <option>Please select</option>
-                    {categories.length > 0 &&
-                        categories.map((c) => (
-                            <option key={c._id} value={c._id}>
-                                {c.name}
-                            </option>
-                        ))}
-                </select>
-            </div>
-            {showSub && (
-                <div>
-                    <label htmlFor="subs">Sub Category</label>
-                    <Select
-                        mode="multiple"
-                        style={{ width: "100%" }}
-                        placeholder="Please select"
-                        value={subs}
-                        onChange={(value) =>
-                            setValues({ ...values, subs: value })
-                        }
-                    >
-                        {subOptions.length &&
-                            subOptions.map((s) => (
-                                <Option key={s._id} value={s._id}>
-                                    {s.name}
-                                </Option>
-                            ))}
-                    </Select>
-                </div>
-            )}
-            <br />
-            <button className="btn btn-outline-info">
-                {loading ? "Loading..." : "Save"}
-            </button>
-        </form>
-    );
+  const { title, description, price, categories, category, subs, shipping, quantity, images, colors, brands, color, brand } =
+    values;
+  return (
+    <form onSubmit={handleSubmit} className="text-white font-weight-bold">
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          className="form-control text-white"
+          value={title}
+          onChange={handleChange}
+          placeholder="Add Product Title"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          className="form-control text-white"
+          placeholder="Add Product Description"
+          value={description}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="price">Price</label>
+        <input
+          type="number"
+          name="price"
+          id="price"
+          className="form-control text-white"
+          placeholder="Add Product Price"
+          value={price}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="shipping">Shipping</label>
+        <select name="shipping" id="shipping" className="form-control text-white" onChange={handleChange}>
+          <option value="No">No</option>
+          <option value="Yes">Yes</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="quantity">Quantity</label>
+        <input
+          type="number"
+          name="quantity"
+          id="quantity"
+          className="form-control text-white"
+          placeholder="Add Product Quantity"
+          value={quantity}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="color">Color</label>
+        <select
+          name="color"
+          id="color"
+          className="form-control text-white"
+          style={{ backgroundColor: '#2c2c6c' }}
+          onChange={handleChange}
+        >
+          <option>Please Select</option>
+          {colors.map((c) => (
+            <option key={c} value={c}>
+              {c}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="brand">Brand</label>
+        <select
+          name="brand"
+          id="brand"
+          className="form-control text-white"
+          style={{ backgroundColor: '#2c2c6c' }}
+          onChange={handleChange}
+        >
+          <option>Please Select</option>
+          {brands.map((b) => (
+            <option key={b} value={b}>
+              {b}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="category">Category</label>
+        <select
+          name="category"
+          id="category"
+          className="form-control text-white"
+          style={{ backgroundColor: '#2c2c6c' }}
+          onChange={handleCategoryChange}
+        >
+          <option>Please select</option>
+          {categories.length > 0 &&
+            categories.map((c) => (
+              <option key={c._id} value={c._id}>
+                {c.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      {showSub && (
+        <div>
+          <label htmlFor="subs">Sub Category</label>
+          <Select
+            mode="multiple"
+            style={{ width: '100%' }}
+            placeholder="Please select"
+            value={subs}
+            onChange={(value) => setValues({ ...values, subs: value })}
+          >
+            {subOptions.length &&
+              subOptions.map((s) => (
+                <Option key={s._id} value={s._id}>
+                  {s.name}
+                </Option>
+              ))}
+          </Select>
+        </div>
+      )}
+      <br />
+      <button style={{ color: '#2c2c6c', letterSpacing: '1px' }} className="btn mt-2 bg-white font-weight-bold btn-raised">
+        {loading ? 'Loading...' : 'Save'}
+      </button>
+    </form>
+  );
 };
 
 export default ProductCreateForm;
--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -1,108 +1,107 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import AdminNav from "../../../components/nav/AdminNav";
-import { toast } from "react-toastify";
-import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
-import { getCategories, getCategorySubs } from "../../../functions/category";
-import FileUpload from "../../../components/forms/FileUpload";
-import { LoadingOutlined } from "@ant-design/icons";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import AdminNav from '../../../components/nav/AdminNav';
+import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { createProduct } from '../../../functions/product';
+import ProductCreateForm from '../../../components/forms/ProductCreateForm';
+import { getCategories, getCategorySubs } from '../../../functions/category';
+import FileUpload from '../../../components/forms/FileUpload';
+import { LoadingOutlined } from '@ant-design/icons';
 const initialState = {
-    title: "",
-    description: "",
-    price: "",
-    categories: "",
-    category: "",
-    subs: [],
-    shipping: "",
-    quantity: "",
-    images: [],
-    colors: ["Black", "Brown", "Silver", "White", "Blue"],
-    brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
-    color: "",
-    brand: "",
+  title: '',
+  description: '',
+  price: '',
+  categories: '',
+  category: '',
+  subs: [],
+  shipping: '',
+  quantity: '',
+  images: [],
+  colors: ['Black', 'Brown', 'Silver', 'White', 'Blue'],
+  brands: ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'ASUS'],
+  color: '',
+  brand: '',
 };
 const ProductCreate = () => {
-    const { user } = useSelector((state) => ({ ...state }));
-    const [loading, setLoading] = useState(false);
-    const [imageLoading, setImageLoading] = useState(false);
-    const [values, setValues] = useState(initialState);
-    const [subOptions, setSubOptions] = useState([]);
-    const [showSub, setShowSub] = useState(false);
-    const loadCategories = () => {
-        getCategories().then((c) =>
-            setValues({ ...values, categories: c.data })
-        );
-    };
-    useEffect(() => {
-        loadCategories();
-    }, []);
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
-        createProduct(values, user.token)
-            .then((res) => {
-                console.log("Product res...", res);
-                setLoading(false);
-                window.location.reload();
-                toast.success(`Product created successfully`);
-            })
-            .catch((error) => {
-                console.log(error);
-                toast.error(error.response.data.error);
-                setLoading(false);
-            });
-    };
-    const handleChange = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value });
-    };
-    const handleCategoryChange = (e) => {
-        e.preventDefault();
-        console.log("Category clicked ... ", e.target.value);
-        setValues({ ...values, subs: [], category: e.target.value });
-        getCategorySubs(e.target.value)
-            .then((res) => {
-                setSubOptions(res.data);
-                console.log("getCategorySubs response...", res);
-            })
-            .catch((error) => console.log(error));
-        setShowSub(true);
-    };
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-2">
-                    <AdminNav />
-                </div>
-                <div className="col-md-10">
-                    {imageLoading ? (
-                        <LoadingOutlined className="text-danger h1" />
-                    ) : (
-                        <h4>Product Create</h4>
-                    )}
-                    <hr />
-                    <div className="p-3">
-                        <FileUpload
-                            values={values}
-                            setValues={setValues}
-                            setImageLoading={setImageLoading}
-                        />
-                    </div>
-                    <ProductCreateForm
-                        handleSubmit={handleSubmit}
-                        handleChange={handleChange}
-                        values={values}
-                        loading={loading}
-                        handleCategoryChange={handleCategoryChange}
-                        subOptions={subOptions}
-                        showSub={showSub}
-                        setValues={setValues}
-                    />
-                </div>
-            </div>
+  const { user } = useSelector((state) => ({ ...state }));
+  const [loading, setLoading] = useState(false);
+  const [imageLoading, setImageLoading] = useState(false);
+  const [values, setValues] = useState(initialState);
+  const [subOptions, setSubOptions] = useState([]);
+  const [showSub, setShowSub] = useState(false);
+  const loadCategories = () => {
+    getCategories().then((c) => setValues({ ...values, categories: c.data }));
+  };
+  useEffect(() => {
+    loadCategories();
+  }, []);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createProduct(values, user.token)
+      .then((res) => {
+        console.log('Product res...', res);
+        setLoading(false);
+        window.location.reload();
+        toast.success(`Product created successfully`);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(error.response.data.error);
+        setLoading(false);
+      });
+  };
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const handleCategoryChange = (e) => {
+    e.preventDefault();
+    console.log('Category clicked ... ', e.target.value);
+    setValues({ ...values, subs: [], category: e.target.value });
+    getCategorySubs(e.target.value)
+      .then((res) => {
+        setSubOptions(res.data);
+        console.log('getCategorySubs response...', res);
+      })
+      .catch((error) => console.log(error));
+    setShowSub(true);
+  };
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
         </div>
-    );
+        <div className="col-md-10">
+          {imageLoading ? (
+            <LoadingOutlined className="text-danger h1" />
+          ) : (
+            <h4
+              className="text-white  font-weight-bold font-italic mx-auto my-4"
+              style={{ letterSpacing: '3px', borderBottom: '5px solid #4db5ff', width: 'fit-content' }}
+            >
+              Product Create
+            </h4>
+          )}
+          <hr />
+          <div className="p-3">
+            <FileUpload values={values} setValues={setValues} setImageLoading={setImageLoading} />
+          </div>
+          <ProductCreateForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            values={values}
+            loading={loading}
+            handleCategoryChange={handleCategoryChange}
+            subOptions={subOptions}
+            showSub={showSub}
+            setValues={setValues}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCreate;
