73bb7173f17749a02bfc57a0bded323bda043746
arslanjajja1 <arslanjajja13@gmail.com>
2023-05-28 10:59:57 +0500

    pushing to heroku

--- server.js
+++ server.js
@@ -24,9 +24,13 @@ app.use(cors())
 app.get('/',(req,res)=>res.send("Helloooooooo"))
 fs.readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
 // Port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 if(process.env.NODE_ENV=='production'){
   app.use(express.static("client/build/"))
+  const path = require('path')
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 }
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
