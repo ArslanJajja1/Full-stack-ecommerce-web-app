4edaaa9b9c87f5211d8597e61023536a37562246
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-23 11:18:03 +0500

    home page categories style change from slider to flex

--- client/src/components/category/CategoryList.js
+++ client/src/components/category/CategoryList.js
@@ -5,47 +5,7 @@ import laptopAvatar from '../../assets/images/laptop-avatar.jpg';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-var settings = {
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  autoplay: true,
-  speed: 2000,
-  autoplaySpeed: 2000,
-  cssEase: 'linear',
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
-    },
-    {
-      breakpoint: 650,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 550,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
+
 const CategoryList = ({ smallDevice }) => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -58,13 +18,12 @@ const CategoryList = ({ smallDevice }) => {
     });
   }, []);
   const showCategories = () => (
-    <Slider {...settings} className="mx-auto" style={smallDevice ? { width: '85%' } : { width: '100%' }}>
-      {categories.map((c) => {
+      categories.map((s) => {
         return (
           <Link
-            to={`/category/${c.slug}`}
-            key={c._id}
-            className="col d-flex flex-column justify-content-center align-items-center pointer"
+            to={`/category/${s.slug}`}
+            key={s._id}
+            className="col d-flex flex-column justify-content-center align-items-center items-center pointer"
           >
             <img
               style={
@@ -72,20 +31,19 @@ const CategoryList = ({ smallDevice }) => {
                   ? { width: '60px', height: '60px', borderRadius: '100%' }
                   : { width: '100px', height: '100px', borderRadius: '100%' }
               }
-              src={c.images.length > 0 ? c.images[0].url : laptopAvatar}
+              src={s.images.length > 0 ? s.images[0].url : laptopAvatar}
               alt=""
             />
             <p className={`${smallDevice && 'fs-6'} text-white pt-2 text-uppercase font-weight-bold`}>{`${
-              c.name.length > 10 ? `${c.name.substring(0, 10)}...` : c.name
+              s.name.length > 10 ? `${s.name.substring(0, 10)}...` : s.name
             } `}</p>
           </Link>
         );
-      })}
-    </Slider>
+      })
   );
   return (
     <div className="px-5 w-100 mx-auto">
-      <div className="row d-flex justify-content-center">
+      <div className="row d-flex justify-content-center flex-wrap">
         {loading ? <h4 className="text-white">Loading...</h4> : showCategories()}
       </div>
     </div>
