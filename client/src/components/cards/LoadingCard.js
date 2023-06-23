12e303dec784d7afe99be38cc430f86913674f28
arslanjajja1 <arslanjajja13@gmail.com>
2023-06-23 14:59:04 +0500

    loading card design changes

--- client/src/components/cards/LoadingCard.js
+++ client/src/components/cards/LoadingCard.js
@@ -5,14 +5,14 @@ const LoadingCard = ({ count }) => {
         let totalCards = [];
         for (let i = 0; i < count; i++) {
             totalCards.push(
-                <Card className="col-md-3 m-3" key={i}>
+                <Card className="col-md-2 m-4">
                     <Skeleton active></Skeleton>
                 </Card>
             );
         }
         return totalCards;
     };
-    return <div className="row pb-5">{cards()}</div>;
+    return <div className="row pb-5 ml-auto">{cards()}</div>;
 };
 
 export default LoadingCard;
